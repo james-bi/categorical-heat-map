6355cabab22bc26be983d38901715a5a44e81ed2
James Scott <james@adjinn.com>
2026-01-03 08:11:38 +0000

    messing around with this some more

--- index.js
+++ index.js
@@ -1,94 +1,230 @@
-const dscc = window.dscc;
+!function(e,R){"object"==typeof exports&&"object"==typeof module?module.exports=R():"function"==typeof define&&define.amd?define("dscc",[],R):"object"==typeof exports?exports.dscc=R():e.dscc=R()}(window,function(){return t={},n.m=C={"./src/index.ts":function(e,N,R){"use strict";var i=this&&this.__assign||function(){return(i=Object.assign||function(e){for(var R,C=1,t=arguments.length;C<t;C++)for(var n in R=arguments[C])Object.prototype.hasOwnProperty.call(R,n)&&(e[n]=R[n]);return e}).apply(this,arguments)};Object.defineProperty(N,"__esModule",{value:!0});var _=R("./src/types.ts");!function(e){for(var R in e)N.hasOwnProperty(R)||(N[R]=e[R])}(R("./src/types.ts")),N.getWidth=function(){return document.body.clientWidth},N.getHeight=function(){return document.documentElement.clientHeight},N.getComponentId=function(){var e=new URLSearchParams(window.location.search);if(null!==e.get("dscId"))return e.get("dscId");throw new Error("dscId must be in the query parameters. This is a bug in ds-component, please file a bug: https://github.com/googledatastudio/ds-component/issues/new")};function E(e){return e.type===_.ConfigDataElementType.DIMENSION||e.type===_.ConfigDataElementType.METRIC}function r(e){return e===_.ConfigDataElementType.DIMENSION?-1:1}function a(e){var R=[];e.config.data.forEach(function(e){e.elements.filter(E).forEach(function(e){R.push(e)})});var C,t=(C=function(e,R){return r(e.type)-r(R.type)},R.map(function(e,R){return{item:e,index:R}}).sort(function(e,R){return C(e.item,R.item)||e.index-R.index}).map(function(e){return e.item})),n=[];return t.forEach(function(e){e.value.forEach(function(){return n.push(e.id)})}),n}function o(R){return function(e){var C,t,n={};return t=R,((C=e).length<t.length?C.map(function(e,R){return[e,t[R]]}):t.map(function(e,R){return[C[R],e]})).forEach(function(e){var R=e[0],C=e[1];void 0===n[C]&&(n[C]=[]),n[C].push(R)},{}),n}}N.fieldsByConfigId=function(e){var R=e.fields.reduce(function(e,R){return e[R.id]=R,e},{}),C={};return e.config.data.forEach(function(e){e.elements.filter(E).forEach(function(e){C[e.id]=e.value.map(function(e){return R[e]})})}),C};function U(e){var R={};return(e.config.style||[]).forEach(function(e){e.elements.forEach(function(e){if(void 0!==R[e.id])throw new Error("styleIds must be unique. Your styleId: '"+e.id+"' is used more than once.");R[e.id]={value:e.value,defaultValue:e.defaultValue}})},{}),R}function Y(e){return e.config.themeStyle}function n(e){switch(e){case _.DSInteractionType.FILTER:return _.InteractionType.FILTER}}function s(e){var R=e.config.interactions;return void 0===R?{}:R.reduce(function(e,R){var C=R.supportedActions.map(n),t={type:n(R.value.type),data:R.value.data};return e[R.id]={value:t,supportedActions:C},e},{})}N.tableTransform=function(e){return{tables:(R=e,t=N.fieldsByConfigId(R),n=a(R),E={},r=n.map(function(e){void 0===E[e]?E[e]=0:E[e]++;var R=E[e],C=t[e][R];return i(i({},C),{configId:e})}),(C={})[_.TableType.DEFAULT]={headers:[],rows:[]},o=C,R.dataResponse.tables.forEach(function(e){o[e.id]={headers:r,rows:e.rows}}),o),fields:N.fieldsByConfigId(e),style:U(e),theme:Y(e),interactions:s(e)};var R,C,t,n,E,r,o},N.objectTransform=function(e){return{tables:(t=a(R=e),(C={})[_.TableType.DEFAULT]=[],n=C,R.dataResponse.tables.forEach(function(e){var R=e.rows.map(o(t));e.id===_.TableType.DEFAULT?n[e.id]=R:(void 0===n[e.id]&&(n[e.id]=[]),n[e.id]=n[e.id].concat(R))}),n),fields:N.fieldsByConfigId(e),style:U(e),theme:Y(e),interactions:s(e)};var R,C,t,n};function u(e){var R,C=!1;return e===N.tableTransform||e===N.objectTransform?C=!0:(R=!1,"identity"===e("identity")&&(R=!0,console.warn("This is an unsupported data format. Please use one of the supported transforms:\n       dscc.objectFormat or dscc.tableFormat.")),R&&(C=!0)),C}N.subscribeToData=function(R,C){if(u(C.transform)){var e=function(e){e.data.type===_.MessageType.RENDER?R(C.transform(e.data)):console.error("MessageType: "+e.data.type+" is not supported by this version of the library.")};window.addEventListener("message",e);var t={componentId:N.getComponentId(),type:_.ToDSMessageType.VIZ_READY};return window.parent.postMessage(t,"*"),function(){return window.removeEventListener("message",e)}}throw new Error("Only the built in transform functions are supported.")},N.sendInteraction=function(e,R,C){var t=N.getComponentId(),n={type:_.ToDSMessageType.INTERACTION,id:e,data:C,componentId:t};window.parent.postMessage(n,"*")},N.clearInteraction=function(e,R){N.sendInteraction(e,R,void 0)}},"./src/types.ts":function(e,R,C){"use strict";var t,n,E,r,o,N;Object.defineProperty(R,"__esModule",{value:!0}),(t=R.ConceptType||(R.ConceptType={})).METRIC="METRIC",t.DIMENSION="DIMENSION",(R.MessageType||(R.MessageType={})).RENDER="RENDER",(n=R.FieldType||(R.FieldType={})).YEAR="YEAR",n.YEAR_QUARTER="YEAR_QUARTER",n.YEAR_MONTH="YEAR_MONTH",n.YEAR_WEEK="YEAR_WEEK",n.YEAR_MONTH_DAY="YEAR_MONTH_DAY",n.YEAR_MONTH_DAY_HOUR="YEAR_MONTH_DAY_HOUR",n.QUARTER="QUARTER",n.MONTH="MONTH",n.WEEK="WEEK",n.MONTH_DAY="MONTH_DAY",n.DAY_OF_WEEK="DAY_OF_WEEK",n.DAY="DAY",n.HOUR="HOUR",n.MINUTE="MINUTE",n.DURATION="DURATION",n.COUNTRY="COUNTRY",n.COUNTRY_CODE="COUNTRY_CODE",n.CONTINENT="CONTINENT",n.CONTINENT_CODE="CONTINENT_CODE",n.SUB_CONTINENT="SUB_CONTINENT",n.SUB_CONTINENT_CODE="SUB_CONTINENT_CODE",n.REGION="REGION",n.REGION_CODE="REGION_CODE",n.CITY="CITY",n.CITY_CODE="CITY_CODE",n.METRO_CODE="METRO_CODE",n.LATITUDE_LONGITUDE="LATITUDE_LONGITUDE",n.NUMBER="NUMBER",n.PERCENT="PERCENT",n.TEXT="TEXT",n.BOOLEAN="BOOLEAN",n.URL="URL",n.IMAGE="IMAGE",n.CURRENCY_AED="CURRENCY_AED",n.CURRENCY_ALL="CURRENCY_ALL",n.CURRENCY_ARS="CURRENCY_ARS",n.CURRENCY_AUD="CURRENCY_AUD",n.CURRENCY_BDT="CURRENCY_BDT",n.CURRENCY_BGN="CURRENCY_BGN",n.CURRENCY_BOB="CURRENCY_BOB",n.CURRENCY_BRL="CURRENCY_BRL",n.CURRENCY_CAD="CURRENCY_CAD",n.CURRENCY_CDF="CURRENCY_CDF",n.CURRENCY_CHF="CURRENCY_CHF",n.CURRENCY_CLP="CURRENCY_CLP",n.CURRENCY_CNY="CURRENCY_CNY",n.CURRENCY_COP="CURRENCY_COP",n.CURRENCY_CRC="CURRENCY_CRC",n.CURRENCY_CZK="CURRENCY_CZK",n.CURRENCY_DKK="CURRENCY_DKK",n.CURRENCY_DOP="CURRENCY_DOP",n.CURRENCY_EGP="CURRENCY_EGP",n.CURRENCY_ETB="CURRENCY_ETB",n.CURRENCY_EUR="CURRENCY_EUR",n.CURRENCY_GBP="CURRENCY_GBP",n.CURRENCY_HKD="CURRENCY_HKD",n.CURRENCY_HRK="CURRENCY_HRK",n.CURRENCY_HUF="CURRENCY_HUF",n.CURRENCY_IDR="CURRENCY_IDR",n.CURRENCY_ILS="CURRENCY_ILS",n.CURRENCY_INR="CURRENCY_INR",n.CURRENCY_IRR="CURRENCY_IRR",n.CURRENCY_ISK="CURRENCY_ISK",n.CURRENCY_JMD="CURRENCY_JMD",n.CURRENCY_JPY="CURRENCY_JPY",n.CURRENCY_KRW="CURRENCY_KRW",n.CURRENCY_LKR="CURRENCY_LKR",n.CURRENCY_LTL="CURRENCY_LTL",n.CURRENCY_MNT="CURRENCY_MNT",n.CURRENCY_MVR="CURRENCY_MVR",n.CURRENCY_MXN="CURRENCY_MXN",n.CURRENCY_MYR="CURRENCY_MYR",n.CURRENCY_NOK="CURRENCY_NOK",n.CURRENCY_NZD="CURRENCY_NZD",n.CURRENCY_PAB="CURRENCY_PAB",n.CURRENCY_PEN="CURRENCY_PEN",n.CURRENCY_PHP="CURRENCY_PHP",n.CURRENCY_PKR="CURRENCY_PKR",n.CURRENCY_PLN="CURRENCY_PLN",n.CURRENCY_RON="CURRENCY_RON",n.CURRENCY_RSD="CURRENCY_RSD",n.CURRENCY_RUB="CURRENCY_RUB",n.CURRENCY_SAR="CURRENCY_SAR",n.CURRENCY_SEK="CURRENCY_SEK",n.CURRENCY_SGD="CURRENCY_SGD",n.CURRENCY_THB="CURRENCY_THB",n.CURRENCY_TRY="CURRENCY_TRY",n.CURRENCY_TWD="CURRENCY_TWD",n.CURRENCY_TZS="CURRENCY_TZS",n.CURRENCY_UAH="CURRENCY_UAH",n.CURRENCY_USD="CURRENCY_USD",n.CURRENCY_UYU="CURRENCY_UYU",n.CURRENCY_VEF="CURRENCY_VEF",n.CURRENCY_VND="CURRENCY_VND",n.CURRENCY_YER="CURRENCY_YER",n.CURRENCY_ZAR="CURRENCY_ZAR",(E=R.TableType||(R.TableType={})).DEFAULT="DEFAULT",E.COMPARISON="COMPARISON",E.SUMMARY="SUMMARY",(r=R.ConfigDataElementType||(R.ConfigDataElementType={})).METRIC="METRIC",r.DIMENSION="DIMENSION",r.MAX_RESULTS="MAX_RESULTS",(o=R.ConfigStyleElementType||(R.ConfigStyleElementType={})).TEXTINPUT="TEXTINPUT",o.SELECT_SINGLE="SELECT_SINGLE",o.CHECKBOX="CHECKBOX",o.FONT_COLOR="FONT_COLOR",o.FONT_SIZE="FONT_SIZE",o.FONT_FAMILY="FONT_FAMILY",o.FILL_COLOR="FILL_COLOR",o.BORDER_COLOR="BORDER_COLOR",o.AXIS_COLOR="AXIS_COLOR",o.GRID_COLOR="GRID_COLOR",o.OPACITY="OPACITY",o.LINE_WEIGHT="LINE_WEIGHT",o.LINE_STYLE="LINE_STYLE",o.BORDER_RADIUS="BORDER_RADIUS",o.INTERVAL="INTERVAL",o.SELECT_RADIO="SELECT_RADIO",(R.DSInteractionType||(R.DSInteractionType={})).FILTER="FILTER",(N=R.ToDSMessageType||(R.ToDSMessageType={})).VIZ_READY="vizReady",N.INTERACTION="vizAction",(R.InteractionType||(R.InteractionType={})).FILTER="FILTER"}},n.c=t,n.d=function(e,R,C){n.o(e,R)||Object.defineProperty(e,R,{enumerable:!0,get:C})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(R,e){if(1&e&&(R=n(R)),8&e)return R;if(4&e&&"object"==typeof R&&R&&R.__esModule)return R;var C=Object.create(null);if(n.r(C),Object.defineProperty(C,"default",{enumerable:!0,value:R}),2&e&&"string"!=typeof R)for(var t in R)n.d(C,t,function(e){return R[e]}.bind(null,t));return C},n.n=function(e){var R=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(R,"a",R),R},n.o=function(e,R){return Object.prototype.hasOwnProperty.call(e,R)},n.p="",n(n.s="./src/index.ts");function n(e){if(t[e])return t[e].exports;var R=t[e]={i:e,l:!1,exports:{}};return C[e].call(R.exports,R,R.exports,n),R.l=!0,R.exports}var C,t});
 
-dscc.subscribeToData(draw, {
-  transform: dscc.objectTransform,
-});
+// Categorical Heatmap for Looker Studio - Visualization Code
+(function() {
+  'use strict';
 
-let map;
-let circles = [];
+  let canvas;
+  let ctx;
+  let width;
+  let height;
 
-function initMap(center) {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: center,
-    mapTypeId: "roadmap",
-  });
-}
+  function createCanvas() {
+    if (canvas) return;
+    
+    canvas = document.createElement('canvas');
+    canvas.id = 'heatmap-canvas';
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';
+    document.body.style.overflow = 'hidden';
+    document.body.appendChild(canvas);
+    
+    ctx = canvas.getContext('2d');
+    
+    const updateSize = () => {
+      width = dscc.getWidth();
+      height = dscc.getHeight();
+      canvas.width = width;
+      canvas.height = height;
+      console.log('Canvas size:', width, 'x', height);
+    };
+    
+    updateSize();
+    window.addEventListener('resize', updateSize);
+  }
 
-function draw(data) {
-  if (!data || !data.tables || !data.tables.DEFAULT.length) return;
+  function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : {r: 255, g: 0, b: 0};
+  }
 
-  const rows = data.tables.DEFAULT;
+  function drawViz(data) {
+    console.log('drawViz called', data);
+    
+    createCanvas();
+    
+    if (!ctx) {
+      console.error('Canvas context not available');
+      return;
+    }
 
-  const points = rows.map(row => ({
-    lat: row[0],
-    lng: row[1],
-    category: row[2],
-    weight: row[3]
-  }));
+    const style = data.style || {};
+    const radius = parseInt(style.circleRadius?.value || style.circleRadius || 50);
+    const maxOpacity = parseFloat(style.maxOpacityValue?.value || style.maxOpacityValue || 0.8);
+    const minOpacity = parseFloat(style.minOpacityValue?.value || style.minOpacityValue || 0.2);
+    
+    const categoryColors = [
+      style.catColor1?.value || style.catColor1 || '#FF6B6B',
+      style.catColor2?.value || style.catColor2 || '#4ECDC4',
+      style.catColor3?.value || style.catColor3 || '#45B7D1',
+      style.catColor4?.value || style.catColor4 || '#FFA07A',
+      style.catColor5?.value || style.catColor5 || '#98D8C8'
+    ];
 
-  const center = { lat: points[0].lat, lng: points[0].lng };
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = '#f5f5f5';
+    ctx.fillRect(0, 0, width, height);
 
-  if (!map) initMap(center);
+    ctx.strokeStyle = '#ddd';
+    ctx.lineWidth = 1;
+    
+    for (let i = 0; i <= 10; i++) {
+      const x = (width / 10) * i;
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+    }
+    
+    for (let i = 0; i <= 10; i++) {
+      const y = (height / 10) * i;
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+      ctx.stroke();
+    }
 
-  clearCircles();
-  renderPoints(points);
-}
+    ctx.fillStyle = '#333';
+    ctx.font = 'bold 16px Arial';
+    ctx.fillText('Categorical Heatmap', 10, 25);
 
-function groupByCategory(points) {
-  const grouped = {};
-  points.forEach(p => {
-    if (!grouped[p.category]) grouped[p.category] = [];
-    grouped[p.category].push(p);
-  });
-  return grouped;
-}
+    const exampleX = width / 2;
+    const exampleY = height / 2;
+    ctx.beginPath();
+    ctx.arc(exampleX, exampleY, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.35)';
+    ctx.fill();
+    ctx.strokeStyle = 'rgba(255, 0, 0, 0.8)';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    
+    ctx.fillStyle = '#000';
+    ctx.font = '12px Arial';
+    ctx.fillText('Example (0,0)', exampleX - 35, exampleY - radius - 5);
 
-const palette = [
-  "#e41a1c",
-  "#377eb8",
-  "#4daf4a",
-  "#984ea3",
-  "#ff7f00",
-  "#ffff33"
-];
+    const tables = data.tables?.DEFAULT;
+    if (!tables || tables.length === 0) {
+      console.log('No data available');
+      ctx.fillStyle = '#666';
+      ctx.font = '14px Arial';
+      ctx.fillText('No data to display. Add data fields to see visualization.', 10, height - 20);
+      return;
+    }
 
-function getCategoryColor(category, index) {
-  return palette[index % palette.length];
-}
+    console.log('Processing', tables.length, 'rows');
 
-function normalizeOpacity(weight, maxWeight) {
-  const minOpacity = 0.05;
-  const maxOpacity = 0.8;
-  return Math.min(
-    maxOpacity,
-    Math.max(minOpacity, weight / maxWeight)
-  );
-}
+    const fields = data.fields || {};
+    const latField = Array.isArray(fields.latitude) ? fields.latitude[0].id : 'latitude';
+    const lngField = Array.isArray(fields.longitude) ? fields.longitude[0].id : 'longitude';
+    const catField = Array.isArray(fields.category) ? fields.category[0].id : 'category';
+    const freqField = fields.frequency ? (Array.isArray(fields.frequency) ? fields.frequency[0].id : 'frequency') : null;
 
-function renderPoints(points) {
-  const grouped = groupByCategory(points);
-  const maxWeight = Math.max(...points.map(p => p.weight));
+    const categories = new Set();
+    let minFreq = Infinity, maxFreq = -Infinity;
+    let minLat = Infinity, maxLat = -Infinity;
+    let minLng = Infinity, maxLng = -Infinity;
 
-  Object.keys(grouped).forEach((category, index) => {
-    const color = getCategoryColor(category, index);
-
-    grouped[category].forEach(point => {
-      const circle = new google.maps.Circle({
-        map: map,
-        center: { lat: point.lat, lng: point.lng },
-        radius: 200,
-        strokeOpacity: 0,
-        fillColor: color,
-        fillOpacity: normalizeOpacity(point.weight, maxWeight)
-      });
-
-      circles.push(circle);
+    tables.forEach(row => {
+      if (row[catField]) categories.add(row[catField]);
+      if (freqField && row[freqField] != null) {
+        const freq = parseFloat(row[freqField]);
+        minFreq = Math.min(minFreq, freq);
+        maxFreq = Math.max(maxFreq, freq);
+      }
+      
+      const lat = parseFloat(row[latField]);
+      const lng = parseFloat(row[lngField]);
+      if (!isNaN(lat) && !isNaN(lng)) {
+        minLat = Math.min(minLat, lat);
+        maxLat = Math.max(maxLat, lat);
+        minLng = Math.min(minLng, lng);
+        maxLng = Math.max(maxLng, lng);
+      }
     });
-  });
-}
 
-function clearCircles() {
-  circles.forEach(c => c.setMap(null));
-  circles = [];
-}
+    const categoryArray = Array.from(categories);
+    const categoryColorMap = {};
+    categoryArray.forEach((cat, idx) => {
+      categoryColorMap[cat] = categoryColors[idx % categoryColors.length];
+    });
+
+    if (!freqField || minFreq === Infinity) {
+      minFreq = 1;
+      maxFreq = 1;
+    }
+
+    const latRange = maxLat - minLat || 1;
+    const lngRange = maxLng - minLng || 1;
+    const padding = 80;
+
+    tables.forEach(row => {
+      const lat = parseFloat(row[latField]);
+      const lng = parseFloat(row[lngField]);
+      const category = row[catField];
+      const frequency = freqField ? parseFloat(row[freqField]) : 1;
+
+      if (isNaN(lat) || isNaN(lng)) return;
+
+      const x = padding + ((lng - minLng) / lngRange) * (width - 2 * padding);
+      const y = height - (padding + ((lat - minLat) / latRange) * (height - 2 * padding));
+
+      let opacity = minOpacity;
+      if (maxFreq > minFreq) {
+        opacity = minOpacity + ((frequency - minFreq) / (maxFreq - minFreq)) * (maxOpacity - minOpacity);
+      } else {
+        opacity = maxOpacity;
+      }
+
+      const color = categoryColorMap[category] || categoryColors[0];
+      const rgb = hexToRgb(color);
+
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${opacity * 0.6})`;
+      ctx.fill();
+      ctx.strokeStyle = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${opacity})`;
+      ctx.lineWidth = 1;
+      ctx.stroke();
+    });
+
+    if (categoryArray.length > 0) {
+      const legendX = width - 150;
+      let legendY = 50;
+      
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.9)';
+      ctx.fillRect(legendX - 5, legendY - 20, 145, categoryArray.length * 25 + 25);
+      ctx.strokeStyle = '#ccc';
+      ctx.strokeRect(legendX - 5, legendY - 20, 145, categoryArray.length * 25 + 25);
+      
+      ctx.fillStyle = '#333';
+      ctx.font = 'bold 12px Arial';
+      ctx.fillText('Categories', legendX, legendY - 5);
+      
+      ctx.font = '11px Arial';
+      categoryArray.forEach((cat, idx) => {
+        const color = categoryColors[idx % categoryColors.length];
+        
+        ctx.fillStyle = color;
+        ctx.fillRect(legendX, legendY + idx * 25, 15, 15);
+        ctx.strokeStyle = '#000';
+        ctx.lineWidth = 1;
+        ctx.strokeRect(legendX, legendY + idx * 25, 15, 15);
+        
+        ctx.fillStyle = '#333';
+        ctx.fillText(cat, legendX + 20, legendY + idx * 25 + 12);
+      });
+    }
+  }
+
+  console.log('Subscribing to data with dscc');
+  dscc.subscribeToData(drawViz, { transform: dscc.objectTransform });
+})();
